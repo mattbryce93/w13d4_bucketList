15c0925120cc9842ae7f7f431f42fad147272ba3
Matthew Bryce <mattbryce93@outlook.com>
2018-08-08 16:38:48 +0100

    Resolved merge issues

--- client/src/app.js
+++ client/src/app.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-// const Request = require('./services/request.js');
 const MapWrapper = require('./services/mapWrapper.js');
-
-const makeRequest = function (url, callback) {
-  const request = new XMLHttpRequest();
-  request.open("GET", url);
-  request.addEventListener('load', callback);
-  request.send();
-};
-=======
 const Request = require('./services/request');
 const ListView = require('./views/listView');
 const DropDown = require('./views/dropDown');
->>>>>>> master
 
 const countryRequest = new Request('https://restcountries.eu/rest/v2/all');
 const dbRequest = new Request('http://localhost:3000/api/countries');
@@ -23,14 +12,8 @@ const populateDropDown = function(countries){
 };
 
 const app = function(){
-<<<<<<< HEAD
   const mapWrapper = new MapWrapper("map", 55.8642, 4.2518, 10);
-
-  const url = 'https://restcountries.eu/rest/v2/all';
-  makeRequest(url, requestComplete);
-=======
   countryRequest.get(populateDropDown);
->>>>>>> master
 };
 
 window.addEventListener('load', app);
